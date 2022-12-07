c863ed59ee6e85b4f8ffe8487b60087ab5ee1fa0
rohit <user@gmail.com>
2022-12-07 23:43:04 +0530

    change into path also add new module cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const port = 3000;
 const student = require('./routes/student.js')
 
 //load router modules
+app.use(cors());
 app.use('/api',student)
 
 
