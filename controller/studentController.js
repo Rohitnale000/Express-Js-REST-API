7dd609bcb445bf367166029ae11745e539541f4a
rohit <user@gmail.com>
2022-12-07 23:47:18 +0530

    to change the get and post method also change in code

--- controller/studentController.js
+++ controller/studentController.js
@@ -1,77 +1,80 @@
+const fs = require("fs");
+const data = require("../Data.json");
+const { v4: uuidv4 } = require("uuid");
 
-const fs = require("fs"); 
-var studentArray = [];
 
-
+// display student data
 const studentDisplay = (req, res) => {
-    res.json(studentArray);
-    console.log(studentArray);
-    fs.writeFile("./Data.json", JSON.stringify(studentArray), (err) => {
-      if (err) {
-        console.log("data is not present");
-      } else {
-        console.log("Display Data success...");
-      }
-    });
-  }
+  res.send(data);
+};
 
-  const studentAdd = (req, res)=> {
-    res.end(JSON.stringify(req.body, null, 2));
-  
-    let studentObject = {
-      id: studentArray.length + 1,
-      first_name: req.body.first_name,
-      email: req.body.email,
-    };
-  
-    studentArray.push(studentObject);
-    console.log(studentArray);
-  
-    //convert studentArray into JSON string
-    let studentJson = JSON.stringify(studentArray);
-    //   //perform file write operation
-    fs.writeFile("./Data.json", studentJson, (error) => {
-      if (error) {
-        console.log("wrong");
-      } else {
-        console.log("data save successfully....");
-      }
-    });
-  }
-
-  const studentUpdate = (req, res) => {
-    let id = req.params.id;
-    let first_name = req.body.first_name;
-    let email = req.body.email;
-  
-    let index = studentArray.findIndex((studentObject) => {
-      return studentObject.id == Number.parseInt(id);
-    });
-    if (index >= 0) {
-      let std = studentArray[index];
-      std.first_name = first_name;
-      std.email = email;
-      res.send(std);
-      console.log(std);
+//save data into the file
+const studentAdd = (req, res) => {
+  res.end(JSON.stringify(req.body, null, 2));
+  const id = uuidv4();
+  let studentObject = {
+    id: id,
+    firstName: req.body.firstName,
+    email: req.body.email,
+  };
+  data.push(studentObject);
+  console.log(data);
+  //convert studentArray into JSON string
+  let studentJson = JSON.stringify(data);
+  //   //perform file write operation
+  fs.writeFile("./Data.json", studentJson, (error) => {
+    if (error) {
+      console.log("wrong");
     } else {
-      res.status(404);
+      console.log("data save successfully....");
     }
-  }
+  });
+};
 
-  const studentDelete = (req, res) => {
-    let id = req.params.id;
-    let index = studentArray.findIndex((studentObject) => {
-      return studentObject.id == Number.parseInt(id);
-    });
-  
-    if (index >= 0) {
-      let deleteStudent = studentArray[index];
-      studentArray.splice(index, 1);
-      res.send(deleteStudent);
-      console.log(deleteStudent);
+
+//update student
+const studentUpdate = (req, res) => {
+  let id = req.params.id;
+  let firstName= req.body.firstName;
+  let email = req.body.email;
+
+  let index = data.findIndex((studentObject) => {
+    return studentObject.id === id;
+  });
+  if (index>=0) {
+    let stud = data[index];
+    stud.firstName = firstName;
+    stud.email = email;
+   
+  } else {
+    res.status("record not found");
+  }
+  let studentJson = JSON.stringify(data);
+  //   //perform file write operation
+  fs.writeFile("./Data.json", studentJson, (error) => {
+    if (error) {
+      console.log("wrong");
     } else {
-      res.status(404);
+      console.log("data save successfully....");
     }
-  }
+    res.send("Record updated Successfully");
+  });
+};
 
-  module.exports = {studentDisplay,studentAdd,studentUpdate,studentDelete};
+//delete specific record
+const studentDelete = (req, res) => {
+  let id = req.params.id;
+  let index = data.findIndex((studentObject) => {
+    return studentObject.id == Number.parseInt(id);
+  });
+
+  if (index >= 0) {
+    let deleteStudent = data[index];
+    data.splice(index, 1);
+    res.send("record has been deleted successfully");
+  } else {
+    res.send("Data Not found");
+  }
+};
+
+module.exports = { studentDisplay, studentAdd, studentUpdate, studentDelete };
